f38bbba38566a8222b7c189c7d74c76136d8b56f
searchablemap <cmurp184@eq.edu.au>
2026-08-11 10:54:56 +1000

    Add files via upload

---
+++ schools.js
@@ -0,0 +1,14 @@
+const schools = [
+  {
+    name: "Logan Adolescent Day Program",
+    shortName: "LADP",
+    minAge: 13,
+    maxAge: 18,
+    programType: "Adolescent Day Program",
+    area: "Logan",
+    address: "Level 1, 55 Wembley Rd, Logan Central QLD 4114",
+    phone: "07 3489 2600",
+    lat: -27.6392,
+    lng: 153.1098
+  }
+];
